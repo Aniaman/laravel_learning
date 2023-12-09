deaa2450a2b4073ddcece7153de527bdae9d3eb0
Animesh Srivastava <animeshsrivastava6199501@gmail.com>
2023-12-09 10:30:18 +0530

    testing

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -31,7 +31,7 @@
               <div class="d-flex">
                 <div class="flex-grow-1">
                   <p class="text-truncate font-size-14 mb-2">Total Sales</p>
-                  <h4 class="mb-2">1520</h4>
+                  <h4 class="mb-2">1830</h4>
                   <p class="text-muted mb-0"><span class="text-success fw-bold font-size-12 me-2"><i class="ri-arrow-right-up-line me-1 align-middle"></i>9.83%</span>from previous period</p>
                 </div>
                 <div class="avatar-sm">
